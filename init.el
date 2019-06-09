3429c3b01274ad795af6be5614b65eeaa3690f07
wanghz <h8d9123@163.com>
2019-06-09 11:10:59 +0800

    yasnippet and yasnipet-snippets

--- init.el
+++ init.el
@@ -36,6 +36,18 @@
 ;;slime repl
 (require 'init-slime)
 (require 'init-editing-utils)
+;;init-yasnippet
+(require 'init-yasnippet)
+
+;;set style to linux
+(setq c-default-style "linux")
+;;automatically indent when press RET
+(global-set-key (kbd "RET") 'newline-and-indent)
+;;use 4 space to indent by default
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+;;align char and {}
+(setq c-basic-offset 4)
 
 (provide 'init)
 
@@ -45,9 +57,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
  '(package-selected-packages
    (quote
-    (session slime-company slime projectile counsel ivy-xref swiper flx company ivy smex scratch helm evil diminish))))
+    (yasnippet-snippets yasnippet session slime-company slime projectile counsel ivy-xref swiper flx company ivy smex scratch helm evil diminish)))
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
